bc63511160fb01636971a8d870af128792c6a8e6
Willmo3 <thelordofbouge@gmail.com>
2023-11-17 21:01:45 -0500

    Add freq retval

--- src/freq.rs
+++ src/freq.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 // Create a hashmap of frequencies in a byte map.
-pub fn gen_frequency(bytes: &Vec<u8>) {
+pub fn gen_frequency(bytes: &Vec<u8>) -> HashMap::<u8, usize> {
     let frequency = bytes.iter().fold(HashMap::<u8, usize>::new(), | mut map, curr | {
         if !map.contains_key(curr) {
             map.insert(curr.clone(), 0);
@@ -9,5 +9,5 @@ pub fn gen_frequency(bytes: &Vec<u8>) {
         map.insert(curr.clone(), map.get(curr).unwrap() + 1);
         map
     });
-    println!("{:?}", frequency); 
+    frequency
 }
