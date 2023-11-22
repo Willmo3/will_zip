8ed2f85c55e307047b497a4b8f2969186d778634
Willmo3 <thelordofbouge@gmail.com>
2023-11-22 17:51:00 -0500

    Simplify node.rs

--- src/node.rs
+++ src/node.rs
@@ -1,19 +1,8 @@
-use std::cmp::Ordering;
-
 // A single node, capable of being filtered into a heap.
 // Author: Will Morris.
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct Node {
-    count: usize,
+    count:  usize,
     value: u8,
-}
-
-// Ordered nature of node is key of binary heap.
-// Explicit implementation needed to handle ties.
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> Ordering {
-        // Ties must be broken or decompression is nondeterministic!
-        &self.count.cmp(other.count)
-            .then_with(|| self.value.cmp(&other.value))    
-    }
 }
