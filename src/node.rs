eb16689ed6bcb0d6f1443b32ceffcd75797dba96
Willmo3 <thelordofbouge@gmail.com>
2023-11-23 08:33:08 -0500

    Add node ord implementations

--- src/node.rs
+++ src/node.rs
@@ -1,7 +1,8 @@
 // A single node, capable of being filtered into a heap.
 // Author: Will Morris.
+use std::cmp::Ordering;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub struct Node {
     value: u8,
     count: usize,
@@ -10,5 +11,20 @@ pub struct Node {
 impl Node {
     pub fn new(value: u8, count: usize) -> Node {
         Self { count: count, value: value }
+    }
+}
+
+// Explicit ord implementation needed to ensure count considered first.
+// default ord implementation would compare based on ordering of struct fields.
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.count.cmp(&other.count)
+            .then_with(|| self.value.cmp(&other.value))
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
