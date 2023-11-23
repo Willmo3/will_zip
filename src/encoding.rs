d0378725ab568cd47390b0afa00cf79dc57c289a
Willmo3 <thelordofbouge@gmail.com>
2023-11-23 08:20:00 -0500

    Simplify gen_encoding

--- src/encoding.rs
+++ src/encoding.rs
@@ -5,9 +5,8 @@ use crate::node;
 // Generate a binary heap of the encoding.
 pub fn gen_encoding (table: &HashMap::<u8, usize>) -> BinaryHeap<node::Node> {
     // Need to vectorize the table.
-    let heap: BinaryHeap<node::Node> = table.iter().fold(BinaryHeap::new(), | mut heap, (byte, count) | {
+    table.iter().fold(BinaryHeap::new(), | mut heap, (byte, count) | {
         heap.push(node::Node::new(byte.clone(), count.clone()));
         heap
-    });
-    heap
+    })
 }
