aabb76b9c985225032ecd9bcb87bf60c43a84d72
Willmo3 <thelordofbouge@gmail.com>
2023-11-24 08:14:31 -0500

    Add frequency counting struct

--- src/freq.rs
+++ src/freq.rs
@@ -1,16 +1,15 @@
 use std::collections::HashMap;
-use crate::node;
+use std::cmp::Ordering;
 
 // Create a hashmap of frequencies in a byte map.
 pub fn gen_frequency(bytes: &Vec<u8>) -> HashMap::<u8, usize> {
-    let frequency = bytes.iter().fold(HashMap::<u8, usize>::new(), | mut map, curr | {
+    bytes.iter().fold(HashMap::<u8, usize>::new(), | mut map, curr | {
         if !map.contains_key(curr) {
             map.insert(curr.clone(), 0);
         }
         map.insert(curr.clone(), map.get(curr).unwrap() + 1);
         map
-    });
-    frequency
+    })
 }
 
 // Given a map of frequencies, "normalize" them so that they are ordered 0-255.
@@ -19,7 +18,7 @@ pub fn gen_frequency(bytes: &Vec<u8>) -> HashMap::<u8, usize> {
 // The fundamental objective of this program is to minimize storage size.
 pub fn normalize(original: &HashMap::<u8, usize>) -> HashMap<u8, u8> {
    let mut storage_vec = original.iter().fold(Vec::new(), | mut vec, (byte, count) | {
-       vec.push(node::Node::new(byte.clone(), count.clone()));
+       vec.push(FreqCount::new(byte.clone(), count.clone()));
        vec
    });
 
@@ -30,4 +29,40 @@ pub fn normalize(original: &HashMap::<u8, usize>) -> HashMap<u8, u8> {
        retmap.insert(storage_vec[i].byte(), i as u8);
    }
    retmap
+}
+
+// A single node, capable of being filtered into a heap.
+// Author: Will Morris.
+
+// A node in this context is a data tuple.
+#[derive(PartialEq, Eq, Debug)]
+pub struct FreqCount {
+    byte: u8,
+    count: usize,
+}
+
+impl FreqCount {
+    pub fn new(byte: u8, count: usize) -> FreqCount {
+        Self { byte: byte, count: count }
+    }
+
+    // Value accessor needed for sorting normalization
+    pub fn byte(&self) -> u8 {
+        self.byte.clone()
+    }
+}
+
+// Explicit ord implementation needed to ensure count considered first.
+// default ord implementation would compare based on ordering of struct fields.
+impl Ord for FreqCount {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.count.cmp(&other.count)
+            .then_with(|| self.byte.cmp(&other.byte))
+    }
+}
+
+impl PartialOrd for FreqCount {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
