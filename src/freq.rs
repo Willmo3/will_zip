fe8dab711057b277e482329c6b3130ec31f8f619
Willmo3 <thelordofbouge@gmail.com>
2023-11-24 07:38:46 -0500

    Add frequency map normalization

--- src/freq.rs
+++ src/freq.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use crate::node;
 
 // Create a hashmap of frequencies in a byte map.
 pub fn gen_frequency(bytes: &Vec<u8>) -> HashMap::<u8, usize> {
@@ -10,4 +11,23 @@ pub fn gen_frequency(bytes: &Vec<u8>) -> HashMap::<u8, usize> {
         map
     });
     frequency
+}
+
+// Given a map of frequencies, "normalize" them so that they are ordered 0-255.
+// Note that this WILL work, as there are only 255 u8 values.
+// The reason we're doing this is that storing a hashmap of u8-u8 is smaller.
+// The fundamental objective of this program is to minimize storage size.
+pub fn normalize(original: &HashMap::<u8, usize>) -> HashMap<u8, u8> {
+   let mut storage_vec = original.iter().fold(Vec::new(), | mut vec, (byte, count) | {
+       vec.push(node::Node::new(byte.clone(), count.clone()));
+       vec
+   });
+
+   storage_vec.sort();
+
+   let mut retmap: HashMap::<u8, u8> = HashMap::new();
+   for i in 0..storage_vec.len() {
+       retmap.insert(storage_vec[i].value(), i as u8);
+   }
+   retmap
 }
