8a5956df6ac71c280c8722488dcb1ea84c788fd4
Willmo3 <thelordofbouge@gmail.com>
2023-11-24 08:16:12 -0500

    Switch encoding to use freq data tuple

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,12 +1,12 @@
 use std::collections::BinaryHeap;
 use std::collections::HashMap;
-use crate::node;
+use crate::freq;
 
 // Generate a binary heap of the encoding.
-pub fn gen_encoding (table: &HashMap::<u8, usize>) -> BinaryHeap<node::Node> {
+pub fn gen_encoding (table: &HashMap::<u8, usize>) -> BinaryHeap<freq::FreqCount> {
     // Need to vectorize the table.
     table.iter().fold(BinaryHeap::new(), | mut heap, (byte, count) | {
-        heap.push(node::Node::new(byte.clone(), count.clone()));
+        heap.push(freq::FreqCount::new(byte.clone(), count.clone()));
         heap
     })
 }
