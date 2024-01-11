740be6e83017ad143eb70364e04d146678f11f33
Willmo3 <thelordofbouge@gmail.com>
2024-01-11 11:35:50 -0500

    Simplify huffman.rs imports

--- src/tree/huffman.rs
+++ src/tree/huffman.rs
@@ -1,19 +1,19 @@
 use std::collections::{BinaryHeap, HashMap};
-use crate::{freq, tree};
-use tree::node;
+use crate::freq::FreqCount;
+use crate::tree::node::{internal, leaf, Node};
 
 /// Prepare a Huffman tree from a given frequency map.
 /// Return the root of the tree if any items or present,
 /// Or nothing otherwise.
 /// NOTE that this expects a normalized frequency -- so will only take in u8!
-pub fn prepare_huffman(frequency: &HashMap::<u8, u8>) -> Option<node::Node> {
+pub fn prepare_huffman(frequency: &HashMap::<u8, u8>) -> Option<Node> {
     // Prepare base heap with all elements sorted by frequency.
     // These are all the leaf nodes.
     let mut heap = frequency.iter().fold(
             BinaryHeap::new(), | mut heap, (byte, count) | {
 
-        let freq = freq::FreqCount::new(*byte, *count as usize);
-        heap.push(node::leaf(freq));
+        let freq = FreqCount::new(*byte, *count as usize);
+        heap.push(leaf(freq));
         heap
     });
 
@@ -21,7 +21,7 @@ pub fn prepare_huffman(frequency: &HashMap::<u8, u8>) -> Option<node::Node> {
     while heap.len() > 1 {
         let left = heap.pop().unwrap();
         let right = heap.pop().unwrap();
-        let highest = node::internal(Box::from(left), Box::from(right));
+        let highest = internal(Box::from(left), Box::from(right));
         heap.push(highest);
     }
 
