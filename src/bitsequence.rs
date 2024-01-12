11859d8c067438473a4901266532af5463a73f55
Willmo3 <thelordofbouge@gmail.com>
2024-01-12 15:15:38 -0500

    Fix bug in bitsequence append; early work on iterator

--- src/bitsequence.rs
+++ src/bitsequence.rs
@@ -24,28 +24,55 @@ impl BitSequence {
     // Append a single bit to the end of the sequence.
     fn append_bit(&mut self, bit: u8) {
         assert!(bit == 0 || bit == 1);
-        self.num_bits += 1;
 
         let byte_index = self.num_bits / 8;
         if byte_index >= self.bytes.len() as u64 {
             self.bytes.push(0);
         }
 
-        let bit_index = self.num_bits % 8;
-        let mask = 1 << bit_index;
-        self.bytes[byte_index as usize] |= mask;
+        if bit != 0 {
+            let bit_index = self.num_bits % 8;
+            let mask = 1 << bit_index;
+            self.bytes[byte_index as usize] |= mask;
+        }
+
+        self.num_bits += 1;
     }
 
     // Get the bit at index usize.
-    fn get_bit(&self, index: u64) -> u8 {
-        assert!(index < self.num_bits);
+    fn get_bit(&self, index: u64) -> Option<u8> {
+        if index >= self.num_bits {
+            return None;
+        }
         let byte_index = index / 8;
         let bit_index = index % 8;
         let mask = 1 << bit_index;
         match mask & self.bytes.get(byte_index as usize).unwrap() {
-            0 => { 0 },
-            _ => { 1 },
+            0 => { Some(0) },
+            _ => { Some(1) },
         }
+    }
+
+    // Get immutable iterator over this sequence.
+    fn iter(&self) -> BitIterator {
+        BitIterator { sequence: &self, index: 0 }
+    }
+}
+
+// Many operations will operate over entire sequences.
+// For this reason, a BitIterator is provided.
+struct BitIterator<'a> {
+    sequence: &'a BitSequence,
+    index: u64,
+}
+
+impl<'a> Iterator for BitIterator<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let retval = self.sequence.get_bit(self.index);
+        self.index += 1;
+        retval
     }
 }
 
@@ -65,15 +92,28 @@ mod tests {
         seq.append_bit(0);
         seq.append_bit(1);
         seq.append_bit(0);
-        assert_eq!(0, seq.get_bit(0));
-        assert_eq!(1, seq.get_bit(1));
+        assert_eq!(0, seq.get_bit(0).unwrap());
+        assert_eq!(1, seq.get_bit(1).unwrap());
 
         for _ in 0..32 {
             seq.append_bit(1);
         }
-        assert_eq!(1, seq.get_bit(32));
-        assert_eq!(1, seq.get_bit(1));
-        assert_eq!(0, seq.get_bit(0));
+        assert_eq!(1, seq.get_bit(32).unwrap());
+        assert_eq!(1, seq.get_bit(1).unwrap());
+        assert_eq!(0, seq.get_bit(0).unwrap());
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut seq = BitSequence::new();
+        for i in 0..64 {
+            seq.append_bit(i % 2);
+        }
+
+        let mut iter = seq.iter();
+        for i in 0..64 {
+            assert_eq!(iter.next().unwrap(), i % 2);
+        }
     }
 }
 
