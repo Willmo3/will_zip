1548975315ba7cc02baa4771f1b12ea301345631
Willmo3 <thelordofbouge@gmail.com>
2024-01-29 12:33:26 -0500

    Add Bytestream LONG_SIZE and long conversion

--- src/file/bytestream.rs
+++ src/file/bytestream.rs
@@ -2,6 +2,8 @@
 // This represents any data that can be constructed from a stream of bytes.
 // This will be used for efficient serialization.
 
+use std::mem::size_of;
+
 pub trait ByteStream {
     type Data;
 
@@ -13,4 +15,15 @@ pub trait ByteStream {
     // Typically, converting into a stream is the last step before file serialization.
     // However, if you need self back, from_stream will work on a proper implementation.
     fn to_stream(self) -> Vec<u8>;
+}
+
+pub(crate) const LONG_LEN: usize = size_of::<u64>();
+
+// Given a slice of bytes, convert them into a u64.
+pub(crate) fn slice_to_long(bytes: &[u8]) -> u64 {
+    assert_eq!(bytes.len(), LONG_LEN);
+
+    let mut buf = [0u8; LONG_LEN];
+    buf.copy_from_slice(&bytes[0..]);
+    u64::from_le_bytes(buf)
 }
