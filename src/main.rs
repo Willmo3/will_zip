35393187aa2610ab34bd79cb87c092a9ec437e41
Willmo3 <thelordofbouge@gmail.com>
2024-01-30 14:59:21 -0500

    Add early arg validation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::process;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use getopt::Opt;
+use std::process::{exit, Output};
+use getopt::{Error, Opt};
 use crate::encoding::bitsequence::BitSequence;
 use crate::file::bytestream::ByteStream;
 use crate::file::wzfile::Wzfile;
@@ -41,24 +42,38 @@ mod file {
 }
 
 fn main() {
-    // All arguments.
     let mut input_file: Option<String> = None;
     let mut output_file: Option<String> = None;
     let mut zip = false;
-    let mut unzip = true;
+    let mut unzip = false;
 
-    if !parse_args(&mut input_file, &mut output_file, &mut zip, &mut unzip) {
+    if let Some(exit_code) =
+        parse_args(&mut input_file, &mut output_file, &mut zip, &mut unzip) {
+
         println!("Terminating.");
-        process::exit(1)
+        exit(exit_code)
     };
 
-    // For now, only checking that there is a second arg.
-    // Not complaining if they pass too many.
-    let filename: String = env::args().nth(1).unwrap_or_else(|| {
-        println!("File name not specified! Usage: wz [filename]");
-        process::exit(1)
-    });
+    if let Some(exit_code) =
+        validate_args(&input_file, &output_file, &zip, &unzip) {
 
+        println!("Terminating.");
+        exit(exit_code)
+    };
+
+    let input_file: String = input_file.unwrap();
+    let output_file: String = output_file.unwrap();
+    // We've validated that zip or unzip must be true.
+    // So no need to check unzip here -- if not zip, then go!
+    if zip {
+        exit(compress(&input_file, &output_file))
+    } else {
+        exit(decompress(&input_file, &output_file))
+    }
+
+    // For now, only checking that there is a second arg.
+
+    /*
     let bytes: Vec<u8> = fs::read(&filename).unwrap_or_else(|_| {
         println!("File not found: {}", &filename);
         process::exit(1)
@@ -79,41 +94,92 @@ fn main() {
     let bytes = Wzfile::new(ordering, seq).to_stream();
 
     let mut output = File::create("test.wz").unwrap();
-    output.write_all(&bytes).unwrap();
+    output.write_all(&bytes).unwrap(); */
 }
+
+
+// ****** FILE COMPRESSOR ****** //
+
+// Returns exit status of program
+fn compress(input_filename: &str, output_filename: &str) -> i32 {
+    0
+}
+
+
+// ****** FILE DECOMPRESSOR ****** //
+
+// Returns exit status of program
+fn decompress(input_filename: &str, output_filename: &str) -> i32 {
+    0
+}
+
+
+
+
+// ****** ARGUMENT CHECKERS ****** //
 
 // Parses args.
 // Grabs the input and output filenames, if applicable.
 // Grabs whether the input file is being zipped or unzipped.
-// Return whether the program should continue.
+// Return either the exit code the program should give, or none.
 fn parse_args(input_filename: &mut Option<String>,
               output_filename: &mut Option<String>,
               zip: &mut bool,
-              unzip: &mut bool) -> bool {
+              unzip: &mut bool) -> Option<i32> {
 
     let args: Vec<String> = env::args().collect();
-    let mut ops = getopt::Parser::new(&args, "zxi:o:");
+    let mut ops = getopt::Parser::new(&args, "uzxi:o:");
     loop {
-        match ops.next().transpose().unwrap() {
-            None => break,
-            Some(opt) => match opt {
-                Opt('z', None)             => { *zip = true; }
-                Opt('x', None)             => { *unzip = true; }
-                Opt('i', Some(str)) => { *input_filename = Some(str.clone()); }
-                Opt('o', Some(str)) => { *output_filename = Some(str.clone()); }
-                Opt('u', None) => {
-                    usage();
-                    return false;
+        match ops.next().transpose() {
+            Ok(result) => match result {
+                None => break,
+                Some(opt) => match opt {
+                    Opt('z', None)             => { *zip = true; }
+                    Opt('x', None)             => { *unzip = true; }
+                    Opt('i', Some(str)) => { *input_filename = Some(str.clone()); }
+                    Opt('o', Some(str)) => { *output_filename = Some(str.clone()); }
+                    Opt('u', None) => {
+                        usage();
+                        return Some(0);
+                    }
+                    _ => {
+                        usage();
+                        return Some(1);
+                    }
                 }
-                Opt(value, _) => {
-                    println!("Unrecognized arg: {}", value);
-                    usage();
-                    return false;
-                }
+            }
+            Err(_) => {
+                usage();
+                return Some(1);
             }
         }
     }
-    true
+    None
+}
+
+// Check whether the provided combination of arguments is valid.
+// If so, return none, indicating no exit code.
+// If the args are invalid, return the program's exit code.
+fn validate_args(input_filename: &Option<String>,
+                 output_filename: &Option<String>,
+                 zip: &bool,
+                 unzip: &bool) -> Option<i32> {
+    if input_filename.is_none() {
+        println!("Input file not specified!");
+        usage();
+        return Some(1)
+    }
+    if output_filename.is_none() {
+        println!("Output file not specified!");
+        usage();
+        return Some(1)
+    }
+    if zip == unzip {
+        println!("Must either zip or unzip a file!");
+        usage();
+        return Some(1)
+    }
+    None
 }
 
 fn usage() {
