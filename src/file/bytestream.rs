8df96f437f23bf80cc594d39a03d4ae6abccc3a7
willmo3 <thelordofbouge@gmail.com>
2024-03-14 10:45:55 -0400

    Switch slice to long to use arbitrary length

--- src/file/bytestream.rs
+++ src/file/bytestream.rs
@@ -19,11 +19,43 @@ pub trait ByteStream {
 
 pub(crate) const LONG_LEN: usize = size_of::<u64>();
 
+// TODO: add a long to slice?
+// Need to have wrappers for the variable length fields.
+
 // Given a slice of bytes, convert them into a u64.
 pub(crate) fn slice_to_long(bytes: &[u8]) -> u64 {
-    assert_eq!(bytes.len(), LONG_LEN);
-
     let mut buf = [0u8; LONG_LEN];
-    buf.copy_from_slice(&bytes[0..]);
+    for i in 0..bytes.len() {
+        buf[i] = bytes[i]
+    }
     u64::from_le_bytes(buf)
+}
+
+// Get the minimum number of bytes needed to represent a 64-bit value.
+pub(crate) fn min_byte_size(value: u64) -> u8 {
+    let data_bytes = value.to_be_bytes();
+
+    // How many leading zeros do we have?
+    // These could just as easily be ignored.
+    let mut leading_zeros = 0;
+    for byte in data_bytes {
+        if byte != 0 {
+            break
+        }
+        leading_zeros += 1
+    }
+
+    return (LONG_LEN - leading_zeros) as u8
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::file::bytestream::slice_to_long;
+
+    #[test]
+    fn test_slice_to_long() {
+        let data = vec![1, 1];
+        let value = slice_to_long(&data);
+        assert_eq!(257, value)
+    }
 }
